fe5fd7880c77f507295ad8d96edb81ef74dabca2
Arteom0 <142208261+Arteom0@users.noreply.github.com>
2023-09-11 20:02:56 +0300

    Add files via upload

---
+++ lesson-79/JS/main.js
@@ -0,0 +1,11 @@
+const meAge = 17
+const correctAge = 18
+
+if (meAge > correctAge) {
+    const result = 'совершеннолетний'
+    console.log(result)
+} else {
+    const result = 'не совершеннолетний'
+    console.log(result)
+}
+
