993334cbd57f15b71e28ce12d9681d99fcd83c7e
Arteom0 <142208261+Arteom0@users.noreply.github.com>
2023-09-12 22:14:24 +0300

    Add files via upload

---
+++ lesson-81/JS/main.js
@@ -0,0 +1,15 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 2; <numbers.length ) {
+
+
+//     console.log(numbers[i])
+// }
+
+
+
+
+const colors = [ Red, Orange, Yellow, Green, LightBlue, Blue, Purple ]
+
+for (let i = -0; i < colors.length;) {
+    console.log(colors[i])
+}
