105eb6549ffc4d39cb24f57b29d6e88d0135f880
Arteom0 <142208261+Arteom0@users.noreply.github.com>
2023-09-12 21:36:22 +0300

    Add files via upload

---
+++ lesson-80/JS/main.js
@@ -0,0 +1,22 @@
+
+// Задание 1
+
+// for (let i = 2; i <= 12; i = i + 2) {
+//     console.log(i)
+// }
+
+// задание 2 не смог выполнить:(
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// задание 3
+
+let i = 2
+
+
+while (i < 12) {
+    i = i + 2
+    console.log(i)
+}
