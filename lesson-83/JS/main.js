217d3aa7ba189b784fd614b0b56b6c752bd36192
Arteom0 <142208261+Arteom0@users.noreply.github.com>
2023-09-14 21:24:44 +0300

    Add files via upload

---
+++ lesson-83/JS/main.js
@@ -0,0 +1,13 @@
+const user = {
+    name: 'Артём',
+    age: 17,
+    height: 180,
+    sayHello(name) {
+        console.log(`Hello ${name}`)
+    }
+}
+console.log(user)
+user.sayHello('name')
+
+
+
